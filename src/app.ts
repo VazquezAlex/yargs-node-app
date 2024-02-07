a3d1429d822cc6ef6f16319ed23adbf708ab4b69
VazquezAlex <alejandrovazquez.dev@gmail.com>
2024-02-07 16:22:45 -0600

    feature: create file with multiplication table

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,26 @@
-console.log("Hola Mundo");
+// Core imports.
+import fs from 'fs';
+
+
+let outputMessage = '';
+const base = 6;
+const headerMessage = `
+================================================
+               Tabla del ${ base }
+================================================
+`;
+
+for (let i = 1; i <= 10; i++) {
+    outputMessage += `${ i } x ${ base } = ${ i * base }\n`;
+}
+
+outputMessage = headerMessage + outputMessage;
+
+const outputPath = `outputs`
+
+// Create the folder if it doesn't exist.
+fs.mkdirSync(outputPath, { recursive: true });
+
+// Save the file.
+fs.writeFileSync(`${ outputPath }/table-${ base }.txt`, outputMessage);
+console.log('File Created');
